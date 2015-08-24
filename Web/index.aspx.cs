78f834835e44d0cf0bfc0fc338fd214eaad8063d
yadnehao1 <312171993@qq.com>
2015-08-24 22:40:49 +0800

    修改一下

---
+++ Web/index.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RuRo.Web
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string username = Common.CookieHelper.GetCookieValue("username");
+            string keshi = Common.CookieHelper.GetCookieValue(username + "department");
+            try
+            {
+                keshi = Common.DEncrypt.DESEncrypt.Decrypt(keshi);
+            }
+            catch (Exception ex)
+            {
+                Common.LogHelper.WriteError(ex);
+                keshi = "";
+            }
+            if (keshi == "")
+            {
+                laName.Text = username;
+            }
+            else
+            {
+                laName.Text = keshi + "/" + username;
+            }
+        }
+    }
+}
