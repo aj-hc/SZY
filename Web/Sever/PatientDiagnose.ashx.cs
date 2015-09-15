015535117d44ffee946ce207af5d0dfb4aaad4a7
alone <zy0803wyl@163.com>
2015-09-16 01:12:21 +0800

    123
    Signed-off-by: alone <zy0803wyl@163.com>

--- Web/Sever/PatientDiagnose.ashx.cs
+++ Web/Sever/PatientDiagnose.ashx.cs
@@ -35,7 +35,9 @@ namespace RuRo.Web.Sever
                     case "qry":/*查询*/
                         QueryData(context, false);
                         break;
-
+                    case "qry2":/*查询*/
+                        QueryData(context);
+                        break;
                     case "post":/*提交*/
                         PostData(context);
                         break;
@@ -43,6 +45,15 @@ namespace RuRo.Web.Sever
             }
             else
                 QueryData(context, true);
+        }
+
+        private void QueryData(HttpContext context)
+        {
+            string code = context.Request.Params["code"].Trim();
+            string codeType = context.Request.Params["codeType"].Trim();
+            string ksrq00 = context.Request.Params["ksrq00"];
+            string jsrq00 = context.Request.Params["jsrq00"];
+            string username = Common.CookieHelper.GetCookieValue("username").Trim();
         }
 
         #region form提交过来的数据
